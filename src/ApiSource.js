ac66c890c8a690b5d34d04c5e8f6e9fbe824b2d6
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-05 13:02:01 -0500

    made some changes

--- src/ApiSource.js
+++ src/ApiSource.js
@@ -1,3 +1,4 @@
+
 async function fetchMovies() {
     const url = ('https://imdb236.p.rapidapi.com/api/imdb/most-popular-movies')
     const options = {
@@ -6,22 +7,15 @@ async function fetchMovies() {
             'x-rapidapi-key': "f3885317c5mshd807b3743b809a5p191acdjsn95d3a11b39d5",
             'x-rapidapi-host': "imdb236.p.rapidapi.com"} 
     }
-    try{
+
+    try {
         const repsone = await fetch(url,options)
         const result = await repsone.json();
-        
         const movies = result.slice(0,1)
         
-
         for(let movieIndex = 0; movieIndex < 3; movieIndex++){
-            console.log(result[movieIndex].primaryTitle)
-        }
+            console.log(result[movieIndex].primaryTitle)}
 
     } catch(error) {
-        console.log(error)
-    }
+        console.log(error)}};
 
-}
-
-fetchMovies()
-console.log("test")
