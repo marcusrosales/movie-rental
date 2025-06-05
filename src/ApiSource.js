2e64a0ab19d6dab7bf2d8402ad76771dde4e9f52
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-05 13:15:02 -0500

    made api return 3 movie posters  so far will look into returning more details later.

--- src/ApiSource.js
+++ src/ApiSource.js
@@ -1,4 +1,6 @@
 
+
+
 async function fetchMovies() {
     const url = ('https://imdb236.p.rapidapi.com/api/imdb/most-popular-movies')
     const options = {
@@ -13,9 +15,14 @@ async function fetchMovies() {
         const result = await repsone.json();
         const movies = result.slice(0,1)
         
+        const movie_array = []
+
         for(let movieIndex = 0; movieIndex < 3; movieIndex++){
-            console.log(result[movieIndex].primaryTitle)}
+            //console.log(result[movieIndex].primaryTitle)
+            movie_array.push(result[movieIndex].primaryImage)}
+        return(console.log(movie_array)) 
 
     } catch(error) {
         console.log(error)}};
 
+fetchMovies()
