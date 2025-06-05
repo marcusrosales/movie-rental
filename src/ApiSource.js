ffc1100e7aba05bcde48b098ad261f5dab1fc46e
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-04 23:14:16 -0500

    configured apiSource and am really getting somewhere finnaly

--- src/ApiSource.js
+++ src/ApiSource.js
@@ -1,14 +1,25 @@
 async function fetchMovies() {
-    const url = await fetch('https://imdb236.p.rapidapi.com/api/imdb/top250-movies')
+    const url = ('https://imdb236.p.rapidapi.com/api/imdb/most-popular-movies')
     const options = {
         method: "GET",
         headers: {
             'x-rapidapi-key': "f3885317c5mshd807b3743b809a5p191acdjsn95d3a11b39d5",
             'x-rapidapi-host': "imdb236.p.rapidapi.com"} 
     }
-    const data = await (res.json)
-    return(data)
+    try{
+        const repsone = await fetch(url,options)
+        const result = await repsone.json();
+        
+        const movies = result.slice(0,1)
+        
 
+        for(let movieIndex = 0; movieIndex < 3; movieIndex++){
+            console.log(result[movieIndex].primaryTitle)
+        }
+
+    } catch(error) {
+        console.log(error)
+    }
 
 }
 
