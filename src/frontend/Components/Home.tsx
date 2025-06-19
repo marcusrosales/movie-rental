b462f6f35a5bdcd14282dd89b88e41f18249881e
Marcus <marcusrosalesofficial5@gmail.com>
2025-06-19 14:53:23 -0500

    added nav bar

---
+++ src/frontend/Components/Home.tsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar";
+
+
+
+function Home(){
+    return(<>
+    <NavBar />
+    
+    </>)
+}
---
+++ src/frontend/Components/NavBar.tsx
@@ -0,0 +1,19 @@
+
+function NavBar(){
+    
+    return(<>
+    <nav>
+        <h1>Test</h1>
+    </nav>
+    </>)
+};
+
+
+
+
+
+
+
+
+
+export default NavBar;
